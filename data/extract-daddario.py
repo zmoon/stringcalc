ad4b9b3d4f2cd3d118d99ca6a747710b99808be0
zmoon <zmoon92@gmail.com>
2022-04-08 10:23:28 -0600

    Start work on extracting D'Addario data

---
+++ data/extract-daddario.py
@@ -0,0 +1,15 @@
+"""
+Extract data from the D'Addario string tension chart PDF
+into a more useful format.
+
+The chart is available at:
+https://www.daddario.com/globalassets/pdfs/accessories/tension_chart_13934.pdf
+"""
+import PyPDF2
+ 
+f = open("tension_chart_13934.pdf", "rb")
+pdf = PyPDF2.PdfFileReader(f)
+
+assert pdf.numPages == 14
+
+f.close()
