1acf017bf81fa2bbee6c1f017ad77e95cd85f13c
zmoon <zmoon92@gmail.com>
2022-04-21 21:12:35 -0600

    Initial fret dist print to cl

--- frets.py
+++ frets.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 
 import numpy as np
 import numpy.typing as npt  # v1.21 needed
+import pandas as pd
 
 
 def d_et(n: int | npt.ArrayLike, *, s: float) -> np.float_ | npt.NDArray[np.float_]:
@@ -28,3 +29,48 @@ def d_et(n: int | npt.ArrayLike, *, s: float) -> np.float_ | npt.NDArray[np.floa
         raise ValueError("input fret numbers should be positive")
 
     return s * (1 - 1 / (2**(n / 12)))
+
+
+def d(N: int, *, L: float, method: str = "et") -> pd.DataFrame:
+    """Fret distance DataFrame for `N` frets and scale length `L`."""
+    n = np.arange(1, N + 1)
+
+    if method in {"et"}:
+        d = d_et(n, s=L)
+    else:
+        raise ValueError(f"invalid `method` {method!r}")
+        
+    dd = np.append(d[0], np.diff(d))
+
+    df = pd.DataFrame({"n": n, "d": d, "Δd": dd}).set_index("n")
+    desc = {
+        "n": "fret number",
+        "d": "distance from nut to fret",
+        "Δd": "distance from previous fret to current"
+    }
+    df.attrs.update(col_desc=desc)
+
+    return df
+
+
+if __name__ == "__main__":
+    L = 21
+    N = 19
+    units = "inches"
+    float_format = "%.3f"
+
+    df = d(N, L=L)
+
+    if units.lower() in {"inches", "in", "in.", "\""}:
+        su_l = "\""
+        su_si = "in"
+    elif units.lower() in {"mm"}:
+        su_l = su_si = "mm"
+    else:
+        raise ValueError(f"invalid `units` {units!r}")
+
+    print(f"L = {L}\" (scale length)")
+    print(df.to_string(float_format=float_format))
+    l = max(len(s) for s in df.attrs["col_desc"])
+    for k, v in df.attrs["col_desc"].items():
+        print(f"{k:{l+2}}{v}{'' if k == 'n' else f' ({su_si})'}")
