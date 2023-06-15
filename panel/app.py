5c907df36f0aebaafb39241234a6723068065a96
zmoon <zmoon92@gmail.com>
2023-06-15 09:04:27 -0600

    Slider with pitch name; units in slider value

--- panel/app.py
+++ panel/app.py
@@ -1,4 +1,5 @@
 import hvplot.pandas  # noqa: F401
+from bokeh.models.formatters import PrintfTickFormatter
 from pyabc2 import Pitch
 
 import panel as pn
@@ -12,33 +13,51 @@ TYPE_OPTIONS = ["PB", "PL", "LE", "LEW", "NYL", "NYLW"]
 TYPE_DEFAULTS = ["PB", "PL"]
 
 
-# tension_input = pn.widgets.TextInput(name="Tension", placeholder="Enter tension in lbf...")
 tension_input = pn.widgets.FloatSlider(
-    name="Tension (lbf)", start=1, end=40, step=0.2, value=16, width=int(WIDTH * 2 / 3)
+    name="Tension",
+    start=1,
+    end=40,
+    step=0.2,
+    value=16,
+    width=int(WIDTH * 2 / 3),
+    format=PrintfTickFormatter(format="%.1f lbf"),
 )
 
-# length_input = pn.widgets.TextInput(name="Scale length", placeholder="Enter scale length in inches...")
 length_input = pn.widgets.FloatSlider(
-    name="Scale length (in)", start=10, end=50, step=0.1, value=22.5, width=int(WIDTH * 2 / 3)
+    name="Scale length",
+    start=10,
+    end=50,
+    step=0.1,
+    value=22.5,
+    width=int(WIDTH * 2 / 3),
+    format=PrintfTickFormatter(format='%.1f"'),
 )
 
-# pitch_input = pn.widgets.TextInput(name="Pitch", placeholder="Enter SPN pitch...")
-pkn_input = pn.widgets.DiscreteSlider(
-    name="Pitch (piano key number)", options=list(range(1, 101)), value=35, width=int(WIDTH * 2 / 3)
+pitch_input = pn.widgets.DiscreteSlider(
+    name="Pitch",
+    options=[Pitch(pkn + 8).name for pkn in range(1, 101)],
+    value="D3",
+    width=int(WIDTH * 2 / 3),
 )
 
 types_input = pn.widgets.CheckBoxGroup(
-    name="String types", options=TYPE_OPTIONS, value=TYPE_DEFAULTS, inline=True
+    name="String types",
+    options=TYPE_OPTIONS,
+    value=TYPE_DEFAULTS,
+    inline=True,
 )
 
 n_input = pn.widgets.IntSlider(
-    name="Number of suggestions", start=1, end=10, value=5, width=int(WIDTH / 3)
+    name="Number of suggestions",
+    start=1,
+    end=10,
+    value=5,
+    width=int(WIDTH / 3),
 )
 
 
-@pn.depends(tension_input, length_input, pkn_input, types_input, n_input)
-def table(T, L, pkn, types, n):
-    pitch = Pitch(pkn + 8).name
+@pn.depends(tension_input, length_input, pitch_input, types_input, n_input)
+def table(T, L, pitch, types, n):
     df = suggest_gauge(T=T, L=L, pitch=pitch, types=types, n=n)
 
     return pn.Column(
@@ -50,7 +69,7 @@ def table(T, L, pkn, types, n):
 pn.Column(
     tension_input,
     length_input,
-    pkn_input,
+    pitch_input,
     n_input,
     types_input,
     table,
