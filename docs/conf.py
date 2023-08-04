d9daffa186f01d8a8d92f005ccc8bc8548ba6e20
zmoon <zmoon92@gmail.com>
2023-08-04 16:07:04 -0600

    NumPy intersphinx

--- docs/conf.py
+++ docs/conf.py
@@ -27,6 +27,7 @@ intersphinx_mapping = {
     "python": ("https://docs.python.org/3/", None),
     "pandas": ("https://pandas.pydata.org/docs/", None),
     "pint": ("https://pint.readthedocs.io/en/stable/", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
 }
 
 exclude_patterns = ["_build"]
